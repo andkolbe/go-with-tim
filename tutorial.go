72bb95ff46c4d5b48b3b4cc64a5bc8045abc22cd
Andrew <kolbe1129@gmail.com>
2021-10-19 15:38:20 -0500

    #6

--- tutorial.go
+++ tutorial.go
@@ -4,5 +4,12 @@ import "fmt"
 
 // entry point into our app. Will be called when we run our go program
 func main() {
-	fmt.Println("Hello world")
+	
+	num1 := 9
+	num2 := 4
+	answer := num1 % num2
+	fmt.Printf("%d", answer) // 2
+
 }
+
+// variable - a way of storing and accessing information
